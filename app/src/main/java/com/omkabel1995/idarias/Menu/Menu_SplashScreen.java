ab7ac2d2bd3f6ecff329bb5f7e4e5abdac72a9fe
AsepTrisna <Aseptrisna1995@gmail.com>
2021-03-06 08:57:12 +0700

    template

---
+++ app/src/main/java/com/omkabel1995/idarias/Menu/Menu_SplashScreen.java
@@ -0,0 +1,16 @@
+package com.omkabel1995.idarias.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omkabel1995.idarias.R;
+
+public class Menu_SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__splash_screen);
+    }
+}
